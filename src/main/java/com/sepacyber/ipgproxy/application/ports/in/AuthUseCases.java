d8e0ec724988d09b5907676ba729b22b0e65ef42
Aklil Biru <a.biru@local.sepa-cyber.com>
2022-04-05 08:34:37 +0300

    Cleanup:remove auth related files

--- src/main/java/com/sepacyber/ipgproxy/application/ports/in/AuthUseCases.java
+++
@@ -1,8 +0,0 @@
-package com.sepacyber.ipgproxy.application.ports.in;
-
-import com.sepacyber.ipgproxy.application.ports.in.responses.GetAuthTokenCommandResponse;
-
-public interface AuthUseCases {
-    public GetAuthTokenCommandResponse getAuthToken(GetAuthTokenCommand command);
-    public GetAuthTokenCommandResponse regenerateAuthToken(RegenerateAuthTokenCommand command);
-}
--- src/main/java/com/sepacyber/ipgproxy/provideradapter/ipg/configurer/IpgFactoryMapperConfigurer.java
+++ src/main/java/com/sepacyber/ipgproxy/provideradapter/ipg/configurer/IpgFactoryMapperConfigurer.java
@@ -1,11 +1,7 @@
 package com.sepacyber.ipgproxy.provideradapter.ipg.configurer;
 
-import com.sepacyber.ipgproxy.application.ports.in.GetAuthTokenCommand;
-import com.sepacyber.ipgproxy.application.ports.in.RegenerateAuthTokenCommand;
 import com.sepacyber.ipgproxy.application.ports.in.command.PayWithCardCommand;
 import com.sepacyber.ipgproxy.provideradapter.ipg.IpgChecksumUtil;
-import com.sepacyber.ipgproxy.provideradapter.ipg.auth.AuthTokenRequest;
-import com.sepacyber.ipgproxy.provideradapter.ipg.auth.RegenerateAuthTokenRequest;
 import com.sepacyber.ipgproxy.provideradapter.ipg.payment.request.AuthenticationDto;
 import com.sepacyber.ipgproxy.shared.mapper.ConfigurableMapperConfigurer;
 import lombok.extern.slf4j.Slf4j;
@@ -19,53 +15,7 @@ import org.springframework.stereotype.Component;
 public class IpgFactoryMapperConfigurer implements ConfigurableMapperConfigurer {
     @Override
     public void configure(MapperFactory factory) {
-        factory.classMap(GetAuthTokenCommand.class, AuthTokenRequest.class)
-                .customize(new CustomMapper<GetAuthTokenCommand, AuthTokenRequest>() {
-                    @Override
-                    public void mapAtoB(GetAuthTokenCommand source, AuthTokenRequest destination, MappingContext context) {
-                    destination.setAuthentication(mapperFacade.map(source, AuthTokenRequest.Authentication.class));
-                    destination.setMerchant(mapperFacade.map(source, AuthTokenRequest.Merchant.class));
-                    }
-                }).register();
-
-
-        factory.classMap(RegenerateAuthTokenCommand.class, RegenerateAuthTokenRequest.class)
-                .customize(new CustomMapper<RegenerateAuthTokenCommand, RegenerateAuthTokenRequest>() {
-                    @Override
-                    public void mapAtoB(RegenerateAuthTokenCommand source, RegenerateAuthTokenRequest destination, MappingContext context) {
-                       destination.setAuthentication(mapperFacade.map(source, RegenerateAuthTokenRequest.Authentication.class));
-                       destination.setAuthToken(source.getAuthToken());
-                    }
-                }).register();
-
-        factory.classMap(GetAuthTokenCommand.class, AuthTokenRequest.Authentication.class)
-                .customize(new CustomMapper<GetAuthTokenCommand, AuthTokenRequest.Authentication>() {
-                    @Override
-                    public void mapAtoB(GetAuthTokenCommand source, AuthTokenRequest.Authentication destination, MappingContext context) {
-                    destination.setSKey(source.getIpgSecureKey());
-                    destination.setPartnerId(source.getIpgPartnerId());
-                    }
-                }).register();
-
-
-        factory.classMap(GetAuthTokenCommand.class, AuthTokenRequest.Merchant.class)
-                .customize(new CustomMapper<GetAuthTokenCommand, AuthTokenRequest.Merchant>() {
-                    @Override
-                    public void mapAtoB(GetAuthTokenCommand source, AuthTokenRequest.Merchant destination, MappingContext context) {
-                    destination.setUsername(source.getIpgUsername());
-                    }
-                }).register();
-
-        factory.classMap(RegenerateAuthTokenCommand.class, RegenerateAuthTokenRequest.Authentication.class)
-                .customize(new CustomMapper<RegenerateAuthTokenCommand, RegenerateAuthTokenRequest.Authentication>() {
-                    @Override
-                    public void mapAtoB(RegenerateAuthTokenCommand source, RegenerateAuthTokenRequest.Authentication destination, MappingContext context) {
-                        destination.setPartnerId(source.getIpgPartnerId());
-                    }
-                }).register();
-
-
-        factory.classMap(PayWithCardCommand.class, AuthenticationDto.class)
+            factory.classMap(PayWithCardCommand.class, AuthenticationDto.class)
                 .customize(new CustomMapper<>() {
                     @Override
                     public void mapAtoB(PayWithCardCommand source, AuthenticationDto destination, MappingContext context) {
